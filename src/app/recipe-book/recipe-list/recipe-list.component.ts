033e75af04d63dec70ffdede349766c7d733abfc
vatrushkka <valiakoziyra@gmail.com>
2021-06-22 16:35:09 +0300

    some recipes

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import {Recipe} from 'src/app/models/recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
+  public recipes: Recipe[] = [
+    new Recipe('Name of recipe', 'some description', 'https://media.self.com/photos/60634a7b6fb177c75e545c84/4:3/w_1600%2Cc_limit/Vegetarian-Mapo-Tofu.jpg'),
+    new Recipe('Test recipe', 'some description', 'https://media.self.com/photos/60634a7b6fb177c75e545c84/4:3/w_1600%2Cc_limit/Vegetarian-Mapo-Tofu.jpg')
+  ];
 }
