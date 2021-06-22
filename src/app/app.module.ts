2b2fdfb0a82e3645d1a04b2c8c557a149c1612e0
vatrushkka <valiakoziyra@gmail.com>
2021-06-22 18:29:22 +0300

    fixed and refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipe-book/recipes/recipes.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { ShoppingComponent } from './shopping/shopping/shopping.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
 
@@ -14,9 +16,11 @@ import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shoppin
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipesComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
     RecipeListComponent,
+    ShoppingComponent,
     ShoppingListComponent,
     ShoppingListEditComponent
   ],
