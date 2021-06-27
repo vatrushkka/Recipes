ec09646957a9776b9f71bdb86974d0fdf7862778
vatrushkka <valiakoziyra@gmail.com>
2021-06-27 12:40:39 +0300

    added ingredients to recipes

--- src/app/recipe-book/models/recipe.model.ts
+++ src/app/recipe-book/models/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from 'src/app/shared/models/ingredient.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public image: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, img: string) {
+  constructor(name: string, desc: string, img: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = desc;
     this.image = img;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipe-book/services/recipes.service.ts
+++ src/app/recipe-book/services/recipes.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from '../models/recipe.model';
+import { Ingredient } from 'src/app/shared/models/ingredient.model';
 
 export class RecipesService {
   public recipeItemSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Name of recipe', 'some description', 'https://media.self.com/photos/60634a7b6fb177c75e545c84/4:3/w_1600%2Cc_limit/Vegetarian-Mapo-Tofu.jpg'),
-    new Recipe('Test recipe', 'some description', 'https://media.self.com/photos/60634a7b6fb177c75e545c84/4:3/w_1600%2Cc_limit/Vegetarian-Mapo-Tofu.jpg')
+    new Recipe(
+      'Name of recipe',
+      'some description',
+      'https://media.self.com/photos/60634a7b6fb177c75e545c84/4:3/w_1600%2Cc_limit/Vegetarian-Mapo-Tofu.jpg',
+      [
+        new Ingredient('meat', 2),
+        new Ingredient('apple', 7)
+      ]
+      ),
+    new Recipe(
+      'Test recipe',
+      'some description',
+      'https://media.self.com/photos/60634a7b6fb177c75e545c84/4:3/w_1600%2Cc_limit/Vegetarian-Mapo-Tofu.jpg',
+      [
+        new Ingredient('meat', 7),
+        new Ingredient('apple', 2)
+      ]
+    )
   ];
 
   public getRecipes(): Recipe[] {
