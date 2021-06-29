11ee4697b63d3aaad1213a2cdb3d2c5aafc3f6ab
vatrushkka <valiakoziyra@gmail.com>
2021-06-29 18:59:46 +0300

    recipe edit mode

--- src/app/recipe-book/components/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/components/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, Params } from '@angular/router';
+
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.scss']
 })
 export class RecipeEditComponent implements OnInit {
+  public id: number;
+  public edit = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params.id;
+          this.edit = params.id != null;
+        }
+      );
   }
 
 }
